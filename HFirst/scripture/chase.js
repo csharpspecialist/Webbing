328e1f082d187805777db2a8f7545f478153df69
csharpspecialist <netspec1@hotmail.com>
2017-09-27 01:44:05 -0400

    getting the function working withch6 proj

--- HFirst/scripture/chase.js
+++ HFirst/scripture/chase.js
@@ -6,6 +6,16 @@ function mercury() {
 //planet.innerHTML="Can u smell what the Rock Cooked?";
   //console.log(planet.innerHTML);
 
-document.getElementById('green').innerHTML='Rocker now';
+var corr = document.getElementById('green');
+corr.innerHTML='Rocker now';
 };
-document.getElementById('bow').onclick=mercury;
+//document.getElementById('bow').onclick=mercury;
+function chevy() {
+
+vehicle = { make: "Equinox", year: 2011, vin : [2020444, 369898], color: "ICE blue"  }
+
+//document.getElementById('showing').innerHTML = vehicle.vin[1];
+
+document.getElementById('open').innerHTML = vehicle.make;
+
+};
