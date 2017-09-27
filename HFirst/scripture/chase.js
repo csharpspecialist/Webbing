8b3eeb75d3e232532f4694299e1510088fb5e88c
csharpspecialist <netspec1@hotmail.com>
2017-09-27 01:13:41 -0400

    add ch6 dom file

---
+++ HFirst/scripture/chase.js
@@ -0,0 +1,11 @@
+
+
+
+function mercury() {
+//var planet = document.getElementById('green');
+//planet.innerHTML="Can u smell what the Rock Cooked?";
+  //console.log(planet.innerHTML);
+
+document.getElementById('green').innerHTML='Rocker now';
+};
+document.getElementById('bow').onclick=mercury;
